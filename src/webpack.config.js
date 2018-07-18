03441566cd3b7e98af571244c807e8ef73e4156c
Ranjan Subbiah <rsubbiah@tucows.com>
2018-07-18 18:35:35 -0400

    added webpack and babel as dependencies, but having trouble running webpack

---
+++ src/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: "dist/assets",
+        filename: "bundle.js"
+    }, module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: ['babel-loader'],
+            query: {
+                presets: ['env', 'stage-0', 'react']
+            }
+        }]
+    }
+}
